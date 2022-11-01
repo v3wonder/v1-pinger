5e8dab95c74f5b3d10c8d1aa016e77563623efc2
wonder <116988637+v3wonder@users.noreply.github.com>
2022-11-01 11:20:33 +1100

    Add files via upload

---
+++ Cum Nugget Pinger.bat
@@ -0,0 +1,85 @@
+@echo off
+title cum nugget
+color 0a
+pause
+echo -----------------------------------
+echo -    Stomach#0001 Pinger   -
+echo -----------------------------------                                                                 
+echo                                                                  
+echo                         /:-.                                     
+echo                        -MMMMMmhs+:.                                 
+echo                        sMMMMMMMMMMMmy+-                          
+echo                        NMMMMMMMMMMMMMMMms:`                      
+echo                       :MMMMMMMMMMMMMMMMMMMms.                    
+echo                       yms+::--::/+oyhdNMMNNMNy-                  
+echo                      `N/             ``.:.ydNMNs`                
+echo                      +N                    ``:smm:               
+echo                      ds           `-:-         `+d/              
+echo                     -M-     `:+oydmNMNd/`        `+:             
+echo                     sd     -dMMMMMMMMMMMm+`        .             
+echo                     m+    :NMMMMMMMMMMMMMMd-                     
+echo                    :M`   `mMMMMMMMMMMMMMMMMm.                    
+echo                   yy    sMMMMNho++ymMMMMMMMy                    
+echo                   .N/   +MMMMm:     `-+yNMMMM-                   
+echo                  `om.`./NMMMN- `  `` ` `+MMMMo`                  
+echo                   ds  sNMMMMy            mMMM:                   
+echo                  -M.  dMMMMd.            sMMm                    
+echo                  sd   oMMMM/             oMMN                    
+echo                  N+   `NMMMh             yMMM-                   
+echo                 /N`    mMMMM:            dMMM+                   
+echo                 hy    `NMMMMd:`         `MMMMs                   
+echo                `M:   .sMMMMMMMh.        sMMMM+                   
+echo                +m  `+mMMMMMMMMMN:     -yMMMMMmy+-`               
+echo                ms.omMMMMMMMMMMMMN:  /hNMMMMMMMMMMmy+-`           
+echo              `:MmMMMMMMMMMMMMMMMMm`sMMMMMMMMMMMMMMMMMmy/.        
+echo           :oydMMMMMMMMMMMMMMMMMMMMsMMMMMMMMMMMMMMMMMMMMMMs       
+echo           dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-      
+echo           /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.     
+echo           hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm.    
+echo          .//////////////////////////////////////////////////-    
+echo.
+echo =============================================
+echo = PLEASE WAIT FOR STOMACHS PINGER TO LOAD... =
+echo =============================================
+timeout /t 4 >nul
+cls
+color 0c
+echo.
+echo +++++++++++++++++++++++++++++++++
+echo +-------------------------------+
+echo + WELCOME TO STOMACHS IP PINGER! +
+echo +-------------------------------+
+echo +++++++++++++++++++++++++++++++++
+:a
+echo.
+set /p x=IP TO PING:
+echo.
+echo =======================================
+ping %x%
+echo -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+@ping.exe (ip address here) -t 8 >nul
+echo.
+:choice
+set /p c=Do you want to exit grimy's IP pinger{Y/N}?
+if /I "%c%" EQU "Y" goto :q
+if /I "%c%" EQU "N" goto :w
+goto :choice
+ 
+:q
+echo goodbye :)
+timeout /t 2 >nul
+exit 
+ 
+ 
+:w
+:pornhub
+set /p c=do you want to ping another IP{Y/N}?
+if /I "%C%" EQU "Y" goto :a
+if /I "%C%" EQU "N" goto :p
+goto :pornhub
+ 
+:p
+ 
+echo bye then see you later :)
+timeout /t 2 >nul
+Advertiseme
